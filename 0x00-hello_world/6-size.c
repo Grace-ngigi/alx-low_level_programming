01d91697009fb90335890fede4d4f03ab6df2b62
Grace-ngigi <gracengigi859@gmail.com>
2023-02-16 08:19:22 -0800

    print the sixe of various types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,10 @@ int main(void)
 	int i;
 	float f;
 
-	printf("Size of a char: %lu.\n", sizeof(c));
-	printf("Size of an int: %lu.\n", sizeof(i));
-	printf("Size of a float: %lu.\n", sizeof(f));
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("size of long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(f));
 	return (0);
 }
