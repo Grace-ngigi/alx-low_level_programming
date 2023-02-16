b64274f6630304dbb7e13b5492ed04ed2a296262
Grace-ngigi <gracengigi859@gmail.com>
2023-02-16 07:14:35 -0800

    modify 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
  * main - Prints a sentence to stdout
+ *
+ * Return: Always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
 }
