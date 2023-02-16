c6a9a679a00020837fd690dd9c51290a0482013b
Grace-ngigi <gracengigi859@gmail.com>
2023-02-16 09:33:45 -0800

    print an eact line to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 11);
 	return (1);
 }
