bb43fa5e2bbd8367ce79fdc47525ef588de81c8f
Grace-ngigi <gracengigi859@gmail.com>
2023-02-17 04:23:50 -0800

    print the alphabet in lowercase except a and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stidio.h>
+/**
+ * main - print lowercase alphabetic letters except e and q
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l == 'q' || l == 'e')
+			continue;
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
+
