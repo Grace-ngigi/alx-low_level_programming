5cd22a7090e95574965b2ec9232f4d5730060673
Grace-ngigi <gracengigi859@gmail.com>
2023-02-17 07:03:33 -0800

    print combinations of single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 /**
  * main - print combinations of single digit numbers
- * return: 0
+ * Return: 0
  */
 int main(void)
 {
 	int d;
 
 	for (d = 0; d <= 9; d++)
+	{
 		putchar(d + 'o');
 		if (d < 9)
-			putchar(',');
-	putchar(' ');
+		putchar(',');
+		putchar(' ');
+	}
 putchar('\n');
 return (0);
 }
