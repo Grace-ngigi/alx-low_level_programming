39cecebf3d812cc9694bc8123cf6db0f3397d605
Grace-ngigi <gracengigi859@gmail.com>
2023-02-22 03:51:04 -0800

    print a phrase on stdout

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints a sentence to sdtout
+ * Return: 0
+ *
+ */
+int main(void)
+{
+		char *c  = "_Putchar";
+		int i;
+
+		for (i = 0; c[i] != '\0'; i++)
+		{
+			_putchar(c[i]);
+		}
+		_putchar('\n');
+		return (0);
+}
