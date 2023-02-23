97abf34d58cbb0ae46ea7336f09c8933976fd0b3
Grace-ngigi <gracengigi859@gmail.com>
2023-02-23 09:00:13 -0800

    print the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prins the numbers from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+		_putchar(i);
+	i++;
+	return  (0);
+}
