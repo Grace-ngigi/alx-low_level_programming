6683971f700c922fa4a70b4593c9dcadb709bccb
Grace-ngigi <gracengigi859@gmail.com>
2023-02-23 13:04:06 -0800

    modifications

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,11 @@ void print_most_numbers(void)
 {
 	char i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == '2' || i == '4')
+		if (i == 2 || i == 4)
 			continue;
 		_putchar(i + '0');
 	}
 	_putchar('\n');
-	return (0);
 }
