b2c30d4d78c0f0b1bab78ca32a9b7a1f94d5472f
Grace-ngigi <gracengigi859@gmail.com>
2023-02-23 09:41:27 -0800

    print 10 times numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times numbers from 0 to 14
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	char x = 0, y;
+
+	while (x <= 9)
+	{
+		while (y <= 14)
+		{
+			_putchar(y);
+			y++;
+		}
+		_putchar('\n');
+		x++;
+	}
+}
+
