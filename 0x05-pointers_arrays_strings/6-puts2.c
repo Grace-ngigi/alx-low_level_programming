4554baa6528b7be8529a96b5fd12ecd46b15ef7e
Grace-ngigi <gracengigi859@gmail.com>
2023-02-27 00:28:30 -0800

    prints every other character of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#includdee "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string pram passed to be printed
+ * Return: 0
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+		_putchar(str[i]);
+	_putchar(10);
+}
