a458feff01a5d286d6290dd0fecb57fc230d1bc3
Grace-ngigi <gracengigi859@gmail.com>
2023-02-27 03:43:07 -0800

    Modify task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,6 +12,9 @@ void print_array(int *a, int n)
 	int i;
 
 	for (i = 0; i < n; i++)
-		printf(i ? ", %d" : "%d", a[i]);
+		if (i != n - 1)
+			printf("%d, ,"a[i]);
+		else
+			printf("%d", a[i]);
 	_putchar(10);
 }
