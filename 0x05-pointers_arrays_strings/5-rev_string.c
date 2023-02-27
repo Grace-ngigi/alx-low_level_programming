5ab08e7c34644671058bd376358c274e8b2c3633
Grace-ngigi <gracengigi859@gmail.com>
2023-02-27 00:06:18 -0800

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,18 @@
 #include "main.h"
-#include <string.h>
 /**
  * rev_string - reverses a string
  * @s: arg passed to contain string being reversed
  * Return: 0
+ *
  */
 void rev_string(char *s)
 {
-	_putchar(strrev(s));
+	int i, j, len;
+
+	for (i = 0; s[i] != '\0'; i++)
+		continue;
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+		_putchar(s[j]);
 }
