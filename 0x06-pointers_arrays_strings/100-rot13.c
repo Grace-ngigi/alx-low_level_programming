fa30f701631bca9d1ad93f60a2d6d4f22344d245
Grace-ngigi <gracengigi859@gmail.com>
2023-03-03 12:44:46 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * rot13 - encodes a string using rot13
+ * rot13 - encodes a string usng rot13
  * @s: string to be encoded
- * Return: pointer to the encoded string
+ * Return: encoded string
  */
 char *rot13(char *s)
 {
 	int i, j;
 	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char rot[] "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
