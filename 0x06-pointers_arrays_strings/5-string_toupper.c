147115076b0a9a02c21e1949f695defa6694eb58
Grace-ngigi <gracengigi859@gmail.com>
2023-03-03 01:47:27 -0800

    change lowercase letters to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		if (s[i] > 'a' && s[i] <= 'z')
-			s[i] = s[i] - 'a' + 'A';
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+		i++;
 	}
+	return (s);
 }
