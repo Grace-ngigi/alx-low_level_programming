1e39d36f9e17d2c1c924e023d992e8278a332330
Grace-ngigi <gracengigi859@gmail.com>
2023-03-03 02:12:19 -0800

    capitalize all wrds of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * cap_String - capitalizes all words of a string
+ * @s: string to be capitalized
+ * Return: pointer to the capitalzied string
+ */
+char *cap_String(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' ' ||
+				s[i - 1] == '\t' ||
+				s[i - 1] == '\n' ||
+				s[i - 1] == ',' ||
+				s[i - 1] == ';' ||
+				s[i - 1] == '.' ||
+				s[i - 1] == '!' ||
+				s[i - 1] == '?' ||
+				s[i - 1] == '"' ||
+				s[i - 1] == '(' ||
+				s[i - 1] == ')' ||
+				s[i - 1] == '{' ||
+				s[i - 1] == '}' ||
+				i == 0)
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
