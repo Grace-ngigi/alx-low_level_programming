bb59a41450a1a1f7f85622ae8a0a5c3a123ff820
Grace-ngigi <gracengigi859@gmail.com>
2023-03-03 01:54:19 -0800

    copies a sring

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,10 +12,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	while (j < n)
+	while (i < n)
 	{
 		dest[i] = '\0';
-		j++;
+		i++;
 	}
 	return (dest);
 }
