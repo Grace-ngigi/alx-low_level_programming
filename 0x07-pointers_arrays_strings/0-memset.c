d6f980be3d7ab3c0cf5c2df15c6bbd9b970fac22
Grace-ngigi <gracengigi859@gmail.com>
2023-03-06 06:29:15 -0800

    fill memory with a cosntant type

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: pointer to destination
+ * @b: value to be filled
+ * @n: number of bytes to be filled
+ * Return: pointer to destination
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n--)
+		*s++ = b;
+	return (s);
+}
