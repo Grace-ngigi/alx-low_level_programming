27c1efabfe51d0dc59ea259b3f96b42ab5ffae25
Grace-ngigi <gracengigi859@gmail.com>
2023-03-07 00:53:50 -0800

    modify task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,21 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, length;
+	unsigned int i, j;
 
-	length = 0;
 	for (i = 0; *(s + i) != '\0'; i++)
 	{
 		for (j = 0; *(accept + j) != '\0'; j++)
 		{
 			if (*(s + i) == *(accept + j))
 			{
-				length++;
 				break;
 			}
 		}
-		if (*(s + i) != *(accept + j))
-			return (length);
+		if (*(accept + j) == '\0')
+			return (i);
 	}
+	return (i);
 }
 
