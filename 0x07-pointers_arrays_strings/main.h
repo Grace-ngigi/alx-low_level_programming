e31bb0484bddd84e7c4c25f76d591970efc665ca
Grace-ngigi <gracengigi859@gmail.com>
2023-03-06 23:05:06 -0800

    locate a substring

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ int _putchar(char ch);
 char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 
 #endif /* MAIN_H */
