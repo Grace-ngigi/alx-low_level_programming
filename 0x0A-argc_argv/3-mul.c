6c86d61c56f209e79d5855a4ba1b3646e83e59a2
Grace-ngigi <gracengigi859@gmail.com>
2023-03-10 04:41:34 -0800

    modify 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: args count
@@ -12,16 +11,14 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-	for (i = 1; i < argc; i++)
-	{
-		mul *= atoi(argv[i]);
-		printf("%d\n", mul);
-	}
-	return (0);
+		for (i = 1; i < argc; i++)
+			mul *= atoi(argv[i]);
+	printf("%d\n", mul);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
