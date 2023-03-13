a7286ab9197e74bb0541f741ea480af2432e9eb1
Grace-ngigi <gracengigi859@gmail.com>
2023-03-13 04:47:55 -0700

    create an array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,9 @@ char *create_array(unsigned int size, char c)
 	char *p;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
+
 	p = malloc(sizeof(char) * size);
 	if (p == NULL)
 		return (NULL);
