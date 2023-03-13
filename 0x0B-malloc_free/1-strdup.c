1e874d8097c89ff8c581144ff97ea4fd258e1b7c
Grace-ngigi <gracengigi859@gmail.com>
2023-03-13 07:18:23 -0700

    modify task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,13 +11,17 @@ char *_strdup(char *str)
 	char *p;
 	int i, j;
 
+	/* check if str = null */
 	if (str == NULL)
 		return (NULL);
+	/* get length of str nd allocate memory */
 	for (i = 0; str[i] != '\0'; i++)
 		p = malloc(sizeof(char) * (i + 1));
+	/* check for null pointer */
 	if (p == NULL)
 		return (NULL);
-	for (j = 0; str[j]; j++)
+	/* make a copy of str in p */
+	for (j = 0; str[j] != '\0'; j++)
 		p[j] = str[j];
 	return (p);
 }
