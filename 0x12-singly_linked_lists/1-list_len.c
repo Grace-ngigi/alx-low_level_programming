f36314f0a8f954124f6d22099a0852dcd3f03f1b
Grace-ngigi <gracengigi859@gmail.com>
2023-03-30 10:11:13 -0700

    return the number of elements in a list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns the number of elements i na list
+ * @h: pointer to struct list_t
+ * Return: number of elements in struct list_t
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i;
+
+	i = 0;
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
