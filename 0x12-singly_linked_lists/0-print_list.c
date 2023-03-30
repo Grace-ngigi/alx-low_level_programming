396c9348e95110b765689f0a2261c320e1e52a0c
Grace-ngigi <gracengigi859@gmail.com>
2023-03-30 09:43:51 -0700

    print all the elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,17 +12,14 @@ size_t print_list(const list_t *h)
 {
 	size_t i;
 
-	if (h)
+	for (i = 0; h != NULL; i++)
 	{
-		for (i = 0; h != NULL; i++)
-		{
+		if (h->str == NULL)
+
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
-		}
-	}
-	else
-	{
-		printf("[0] (nil}");
+		else
+			printf("[0] (nil)\n");
+		h = h->next;
 	}
 	return (i);
 }
