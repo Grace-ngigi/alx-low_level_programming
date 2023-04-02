51ca7500ad0985d47d964d6f3971b7cac0c1e602
Grace-ngigi <gracengigi859@gmail.com>
2023-04-02 11:45:59 -0700

    fix typo in lists.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,10 +7,10 @@ struct list
 	unsigned int len;
 	struct list *next;
 }
-typedef struct list list_t;
+typedef struct list list_t
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
-ist_t *add_node(list_t **head, const char *str);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
