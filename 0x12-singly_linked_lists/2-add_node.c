3964bb471508b9b36c433c3657aa946e5cecc936
Grace-ngigi <gracengigi859@gmail.com>
2023-04-02 12:34:41 -0700

    add a missing closing bracket on malloc

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,21 +8,21 @@
  * @str: new string to add in the node
  * Return: the address of new element
  */
-list_t *add_node(list_t **head, counst char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_list;
 	unsigned int i;
 
-	i = 0;
-	while (str[len])
-		len++;
-	new_list = malloc(sizeof(list_t);
+	new_list = malloc(sizeof(list_t));
 	if (new_list == NULL)
 	return (NULL);
 
 	new_list->str = strdup(str);
-	new_list->len = len;
-	new_list->next = (*head);
-	(*head) = new_list;
+	for (i = 0; str[i] != '\0'; i++)
+	continue;
+
+	new_list->len = i;
+	new_list->next = *head;
+	*head = new_list;
 	return (*head);
 }
