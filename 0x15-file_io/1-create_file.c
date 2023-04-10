07a4d6fafd582a3acd2e2c3ed7847f03d0e517aa
Grace-ngigi <gracengigi859@gmail.com>
2023-04-10 04:55:26 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: char pointer to filename
+ * @text_content: string to be written
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, w_file;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w_file = write(fd, text_content, i);
+
+	if (fd == -1 || w_file == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
