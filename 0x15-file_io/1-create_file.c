1f3dfa60dc93f40a11a5b1af60eef6a085c8a895
Grace-ngigi <gracengigi859@gmail.com>
2023-04-10 11:59:12 -0700

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_RDRW | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-	if (text_content)
+	
+	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w_file = write(fd, text_content, i);
-	if (w_file != i)
+	if (w_file == -1 || fd == -1)
 		return (-1);
 	}
 	close(fd);
