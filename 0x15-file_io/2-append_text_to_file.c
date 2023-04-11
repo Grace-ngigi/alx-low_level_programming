637e63c9d10024be899a60ec59b103fb726a8a48
Grace-ngigi <gracengigi859@gmail.com>
2023-04-11 04:05:12 -0700

    modify task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,19 +9,21 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, i, w_file;
 
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, 0600);
+	if (fd == -1)
+		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i] != '\0'; i++)
 			;
-	}
-	if (filename == NULL)
-		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
 	w_file = write(fd, text_content, i);
 
-	if (fd == -1 || w_file == -1)
+	if (w_file == -1)
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
