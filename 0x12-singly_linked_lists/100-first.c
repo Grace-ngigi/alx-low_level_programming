bc7eafb96067939bfa67c1cdba2450bf64ea951d
Grace-ngigi <gracengigi859@gmail.com>
2023-05-20 21:05:50 -0700

    prints before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printSomeText(void) __attribute__ ((constructor));
+/**
+ * printSomeText - prints some text before main is executed
+ * Return : Nothing
+ */
+void printSomeText(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
+}
