a8aaa6ea0daf4d9739126961ffe6aadb84ff38e7
Grace-ngigi <gracengigi859@gmail.com>
2023-06-10 22:42:46 -0700

    retrieve a value from a key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: pointer to the hash table
+ * @key: pointer to the key being retrieved
+ * Return: value retrieved
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int keyIndex;
+
+	keyIndex = key_index((unsigned char *)key, ht->array);
+	if (keyIndex)
+		return (ht->array[keyIndex]->value);
+	else
+		return (NULL);
+}
