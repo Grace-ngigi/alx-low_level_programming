713ac66cda6d1f4127975de1aced55c73db69947
Grace-ngigi <gracengigi859@gmail.com>
2023-06-10 21:12:38 -0700

    create a new hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	newTable->array = malloc(sizeof(hash_table_t) * size);
 	if (newTable->array == NULL)
 		return (NULL);
-	
 	for (i = 0; i < size; i++)
 		;
 	newTable->array[i] = NULL;
