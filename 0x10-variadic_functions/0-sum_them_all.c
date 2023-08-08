0e38ab881771ec27ba97dfa6a5f14170e0a7e6a1
Grace-ngigi <gracengigi859@gmail.com>
2023-08-07 18:55:03 -0700

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,11 +9,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list a;
-	unsigned int i, sum;
+	unsigned int i, sum = 0;
+
+	if (n == 0)
+		return (0);
 
 	va_start(a, n);
-	sum = 0;
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(a, int);
 	va_end(a);
--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,4 +8,41 @@
  */
 void print_all(const char * const format, ...)
 {
+	unsigned int i;
+	char *str;
+	char *separator;
+	va_list argsList;
 
+	va_start(argsList, format);
+	separator = "";
+
+	i  = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(argsList, int));
+				break;
+			case 'i':
+                                printf("%s%d", separator, va_arg(argsList, int));
+                                break;
+			case 'f':
+                                printf("%s%f", separator, va_arg(argsList, double));
+                                break;
+			case 's':
+				str = va_arg(argsList, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		separator = ", ";
+		i++;
+	}
+	printf("\n");
+	va_end(argsList);
+}
