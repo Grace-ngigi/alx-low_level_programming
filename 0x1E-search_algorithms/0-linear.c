0901ff5b7b423ef591f6351c8b190260e1953e67
Grace-ngigi <gracengigi859@gmail.com>
2023-09-12 17:22:58 -0700

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,8 +10,8 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
-	if(size == 0 || array == NULL)
-		return(-1);
+	if (size == 0 || array == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
